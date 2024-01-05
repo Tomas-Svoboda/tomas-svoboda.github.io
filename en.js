6d4634f037e6db11d86a0fa842da90f552110f83
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2024-01-05 16:25:25 +0100

    Update en.js

--- en.js
+++ en.js
@@ -2,7 +2,7 @@ function updateLinkText() {
   var navbar = document.querySelector(".navbar");
   var links = navbar.querySelectorAll("a");
   var screenWidth = window.innerWidth;
-  if (screenWidth < 523) {
+  if (screenWidth < 531) {
     links[0].textContent = "About";
     links[1].textContent = "Skills";
     links[2].textContent = "Career";
