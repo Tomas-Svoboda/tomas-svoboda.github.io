20ccebc5ff39b107c7fa7bcfa879d31e369dc6b6
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2024-01-07 15:55:38 +0100

    Update sv.js

--- js/sv.js
+++ js/sv.js
@@ -9,7 +9,7 @@ function updateLinkText() {
     links[3].textContent = "Skills";
     links[4].textContent = "Certifikat";
     links[5].textContent = "Kontakt";
-  } else if (screenWidth < 630) {
+  } else if (screenWidth < 635) {
     links[0].textContent = "Om mig";
     links[1].textContent = "IT-karriär";
     links[2].textContent = "Konsultuppdrag";
