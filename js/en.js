f3d9028017b739d7adf18e100f026b00b4e85322
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2024-01-07 15:25:06 +0100

    Update en.js

--- js/en.js
+++ js/en.js
@@ -9,9 +9,18 @@ function updateLinkText() {
     links[3].textContent = "Projects";
     links[4].textContent = "Certs";
     links[5].textContent = "Contact";
-  } else {
-    links[0].textContent = "About Me";
+  }
+  else if (screenWidth < 554) {
+    links[0].textContent = "About";
     links[1].textContent = "Skills";
+    links[2].textContent = "Career";
+    links[3].textContent = "Projects";
+    links[4].textContent = "Certs";
+    links[5].textContent = "Contact";
+  } 
+  else {
+    links[0].textContent = "About Me";
+    links[1].textContent = "Skills & Tools";
     links[2].textContent = "IT Career";
     links[3].textContent = "Assignments";
     links[4].textContent = "Certifications";
