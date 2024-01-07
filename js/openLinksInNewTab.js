45c58788085996faaae092a0836cd29ae0cedd73
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2024-01-07 13:54:26 +0100

    Update openLinksInNewTab.js

--- js/openLinksInNewTab.js
+++ js/openLinksInNewTab.js
@@ -3,4 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
   mainLinks.forEach(function (link) {
     link.setAttribute('target', '_blank');
   });
+  link.addEventListener('mouseover', function () {
+    link.classList.add('hovered-link');
+  });
+  link.addEventListener('mouseout', function () {
+    link.classList.remove('hovered-link');
+  });
 });
