5075b7d7dd0e376fe8c69182efd8cff7eadb4644
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2024-01-07 15:34:09 +0100

    Update sv.js

--- js/sv.js
+++ js/sv.js
@@ -4,23 +4,23 @@ function updateLinkText() {
   var screenWidth = window.innerWidth;
   if (screenWidth < 571) {
     links[0].textContent = "Profil";
-    links[1].textContent = "Skills";
-    links[2].textContent = "Karriär";
-    links[3].textContent = "Projekt";
+    links[1].textContent = "Karriär";
+    links[2].textContent = "Projekt";
+    links[3].textContent = "Skills";
     links[4].textContent = "Certifikat";
     links[5].textContent = "Kontakt";
   } else if (screenWidth < 621) {
     links[0].textContent = "Om mig";
-    links[1].textContent = "Kompetens";
-    links[2].textContent = "IT-karriär";
-    links[3].textContent = "Konsultuppdrag";
+    links[1].textContent = "IT-karriär";
+    links[2].textContent = "Konsultuppdrag";
+    links[3].textContent = "Kompetens";
     links[4].textContent = "Certifieringar";
     links[5].textContent = "Kontakt";
   } else {
     links[0].textContent = "Om mig";
-    links[1].textContent = "Kompetens och verktyg";
-    links[2].textContent = "IT-karriär";
-    links[3].textContent = "Konsultuppdrag";
+    links[1].textContent = "IT-karriär";
+    links[2].textContent = "Konsultuppdrag";
+    links[3].textContent = "Kompetens och verktyg";
     links[4].textContent = "Certifieringar";
     links[5].textContent = "Kontakt";
   }
