25a4221f0d8f19bf1fe0f5fd2102ae46a35f5519
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2024-01-07 15:35:16 +0100

    Update en.js

--- js/en.js
+++ js/en.js
@@ -4,25 +4,25 @@ function updateLinkText() {
   var screenWidth = window.innerWidth;
   if (screenWidth < 524) {
     links[0].textContent = "About";
-    links[1].textContent = "Skills";
-    links[2].textContent = "Career";
-    links[3].textContent = "Projects";
+    links[1].textContent = "Career";
+    links[2].textContent = "Projects";
+    links[3].textContent = "Skills";
     links[4].textContent = "Certs";
     links[5].textContent = "Contact";
   }
   else if (screenWidth < 574) {
     links[0].textContent = "About Me";
-    links[1].textContent = "Skills";
-    links[2].textContent = "IT Career";
-    links[3].textContent = "Assignments";
+    links[1].textContent = "IT Career";
+    links[2].textContent = "Assignments";
+    links[3].textContent = "Skills";
     links[4].textContent = "Certifications";
     links[5].textContent = "Contact";
   } 
   else {
     links[0].textContent = "About Me";
-    links[1].textContent = "Skills and Tools";
-    links[2].textContent = "IT Career";
-    links[3].textContent = "Assignments";
+    links[1].textContent = "IT Career";
+    links[2].textContent = "AssSignments";
+    links[3].textContent = "Skills and Tools";
     links[4].textContent = "Certifications";
     links[5].textContent = "Contact";
   }
