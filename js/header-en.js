945ffa0f04faff3827196f6bfb75bebf72bb5b37
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2025-01-13 14:16:12 +0100

    Update header-en.js

--- js/header-en.js
+++ js/header-en.js
@@ -1,17 +1,15 @@
 document.write(`
-  <div class="container">
-    <div class="header">
-      <h1>Tomas Svoboda</h1>
-      <p>Technical Tester and Test Automation Developer at Consid Stockholm</p>
-    </div>
-    <div class="navbar">
-      <a href="./about">About</a>
-      <a href="./career">Career</a>
-      <a href="./projects">Projects</a>
-      <a href="./skills">Skills</a>
-      <a href="./certifications">Certs</a>
-      <a href="./contact">Contact</a>
-      <a href="./"><img src="icons/swedish.png" alt="Swedish" width="20" height="20"></a>
-    </div>
+  <div class="header">
+    <h1>Tomas Svoboda</h1>
+    <p>Technical Tester and Test Automation Developer at Consid Stockholm</p>
+  </div>
+  <div class="navbar">
+    <a href="./about">About</a>
+    <a href="./career">Career</a>
+    <a href="./projects">Projects</a>
+    <a href="./skills">Skills</a>
+    <a href="./certifications">Certs</a>
+    <a href="./contact">Contact</a>
+    <a href="./"><img src="icons/swedish.png" alt="Swedish" width="20" height="20"></a>
   </div>
 `);
