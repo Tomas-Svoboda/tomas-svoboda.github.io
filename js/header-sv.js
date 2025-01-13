679caa263dfd49de08e6b0f29ee639655eb8ce65
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2025-01-13 14:34:12 +0100

    Update header-sv.js

--- js/header-sv.js
+++ js/header-sv.js
@@ -3,13 +3,13 @@ document.write(`
     <h1>Tomas Svoboda</h1>
     <p>Teknisk testare och Testautomatiserare på Consid Stockholm</p>
     </div>
-    <div class="navbar">
-      <a href="./">Profil</a>
-      <a href="./karriar">Karriär</a>
-      <a href="./uppdrag">Projekt</a>
-      <a href="./kompetens">Skills</a>
-      <a href="./certifieringar">Certifikat</a>
-      <a href="./kontakt">Kontakt</a>
-      <a href="./career"><img src="icons/english.png" alt="English" width="20" height="20"></a>
+  <div class="navbar">
+    <a href="./">Profil</a>
+    <a href="./karriar">Karriär</a>
+    <a href="./uppdrag">Projekt</a>
+    <a href="./kompetens">Skills</a>
+    <a href="./certifieringar">Certifikat</a>
+    <a href="./kontakt">Kontakt</a>
+    <a href="./career"><img src="icons/english.png" alt="English" width="20" height="20"></a>
   </div>
 `);
