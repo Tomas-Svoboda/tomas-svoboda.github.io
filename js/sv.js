ef98af5038bea1ecff9faadbf7bc8c49ca2ad864
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2025-01-14 13:34:34 +0100

    Update sv.js

--- js/sv.js
+++ js/sv.js
@@ -2,7 +2,7 @@ function updateLinkText() {
   var navbar = document.querySelector(".navbar");
   var links = navbar.querySelectorAll("a");
   var screenWidth = window.innerWidth;
-  if (screenWidth < 585) {
+  if (screenWidth < 588) {
     links[0].textContent = "Profil";
     links[1].textContent = "Karriär";
     links[2].textContent = "Projekt";
