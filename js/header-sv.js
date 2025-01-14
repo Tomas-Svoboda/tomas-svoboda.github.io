756e98500570cf20951a5662ab77cf2ef79092bf
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2025-01-14 14:42:06 +0100

    Update header-sv.js

--- js/header-sv.js
+++ js/header-sv.js
@@ -10,6 +10,36 @@ document.write(`
     <a href="./kompetens">Skills</a>
     <a href="./certifieringar">Certifikat</a>
     <a href="./kontakt">Kontakt</a>
-    <a href="./about"><img src="icons/english.png" alt="English" width="20" height="20"></a>
+    <a id="en-link" href="#"><img src="icons/english.png" alt="English" width="20" height="20"></a>
+  </div>
+
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const enLink = document.getElementById('en-link');
+        const currentPath = window.location.pathname;
+
+        let newPath = '';
+        switch (currentPath) {
+            case '/karriar':
+                newPath = '/career';
+                break;
+            case '/uppdrag':
+                newPath = '/projects';
+                break;
+            case '/kompetens':
+                newPath = '/skills';
+                break;
+            case '/certifieringar':
+                newPath = '/certifications';
+                break;
+            case '/kontakt':
+                newPath = '/contact';
+                break;
+            default:
+                newPath = '/about';
+        }
+        enLink.href = newPath;
+    });
+  </script>
   </div>
 `);
