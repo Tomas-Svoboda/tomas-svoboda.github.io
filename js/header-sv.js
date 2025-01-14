bbd40d0f71a15ff9e96bd5cd63799f48c9385af0
Tomas Svoboda <151452634+Tomas-Svoboda@users.noreply.github.com>
2025-01-14 15:51:09 +0100

    Update header-sv.js

--- js/header-sv.js
+++ js/header-sv.js
@@ -4,37 +4,18 @@ document.write(`
     <p>Teknisk testare på Consid Stockholm</p>
   </div>
   <div class="navbar">
-    <a href="./">Profil</a>
-    <a href="./karriar">Karriär</a>
-    <a href="./uppdrag">Projekt</a>
-    <a href="./kompetens">Skills</a>
-    <a href="./certifieringar">Certifikat</a>
-    <a href="./kontakt">Kontakt</a>
-    <a id="en-link" href="#"><img src="icons/english.png" alt="English" width="20" height="20"></a>
+    <div class="row">
+      <a href="./">Profil</a>
+      <a href="./karriar">Karriär</a>
+      <a href="./uppdrag">Projekt</a>
+      <a href="./kompetens">Skills</a>
+    </div>
+    <div class="row">
+      <a href="./certifieringar">Certifikat</a>
+      <a href="./kontakt">Kontakt</a>
+      <a id="en-link" href="#"><img src="icons/english.png" alt="English" width="20" height="20"></a>
+    </div>
   </div>
-
-  <script>
-    function adjustNavbar() {
-      var navbar = document.querySelector('.navbar');
-      var skillsLink = document.querySelector('a[href="./kompetens"]');
-      var certLink = document.querySelector('a[href="./certifieringar"]');
-      var br = document.querySelector('.navbar br');
-  
-      if (navbar.scrollWidth > window.innerWidth) {
-        if (!br) {
-          br = document.createElement('br');
-          navbar.insertBefore(br, certLink);
-        }
-      } else {
-        if (br) {
-          navbar.removeChild(br);
-        }
-      }
-    }
-  
-    window.onload = adjustNavbar;
-    window.onresize = adjustNavbar;
-  </script>
 
   <script>
     document.addEventListener('DOMContentLoaded', function() {
